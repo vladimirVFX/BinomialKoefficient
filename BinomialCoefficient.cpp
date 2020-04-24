8f591294158a9cd8189ca84e9e3e8bf2aae5d010
VladimirMironov <44780782+vladimirVFX@users.noreply.github.com>
2020-04-24 23:07:07 +0300

    Add files via upload

---
+++ BinomialCoefficient.cpp
@@ -0,0 +1,31 @@
+﻿// BinomialCoefficient.cpp
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251>nul");
+ 
+    const int n = 10;
+    cout << "Enter N: ";
+   
+    int bnm[n + 1];
+    int k = 0;
+    bnm[0] = 1;
+    cout << bnm[0];
+
+    while (k < n) {
+        bnm[k + 1] = bnm[k] * (n - k) / (k + 1);
+        cout << " " << bnm[k + 1];
+        k++;
+    }
+
+    cout << endl;
+
+    system("pause > nul");
+
+    return 0;
+}
